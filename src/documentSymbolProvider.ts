73e623616f8fd9982e0bbb82537f0e72e0f76482
jaunito76 <clinton.kliethermes@gmail.com>
2020-09-15 22:32:18 -0500

    Updated Starting Tokens w/ Property

--- src/documentSymbolProvider.ts
+++ src/documentSymbolProvider.ts
@@ -12,13 +12,13 @@ export class VbaDocumentSymbolProvider implements vscode.DocumentSymbolProvider 
         console.log('VbaDocumentSymbolProvider.provideDocumentSymbols');
         // [Public | Private | Friend] [Static] Function name [(arglist)] [As type]
         // [Public | Private | Friend] [Static] Sub name [(arglist)]
-        const functionRegex = /^\s*((Public|Private|Friend)\s+)?((Static)\s+)?(Function|Sub|Property)\s+(\w+)/i;
-
+        const regexStart = /^\s*((Public|Private|Friend)\s+)?((Static)\s+)?(Function|Sub|Property)\s+([a-zA-Z])(\w*)/i;
+        const regexEnd =/^\s*(End)\s+(Function|Sub|Property)/i;
         const result: vscode.SymbolInformation[] = [];
 
         for (let line = 0; line < document.lineCount; line++) {
             var text = document.lineAt(line);
-            let matches = text.text.match(functionRegex);
+            let matches = text.text.match(regexStart);
             if (matches) {
                 result.push(
                     new vscode.SymbolInformation(
