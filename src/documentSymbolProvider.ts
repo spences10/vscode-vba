9639a99d1ba3fecf00f9bf372f9a7f4b0304e1ce
mituki-n <wagadao.y@outlook.com>
2021-01-30 11:45:07 +0900

    add definition jump and hover

--- src/documentSymbolProvider.ts
+++ src/documentSymbolProvider.ts
@@ -2,6 +2,7 @@
 //   https://code.visualstudio.com/api/language-extensions/programmatic-language-features#show-all-symbol-definitions-within-a-document
 //   https://github.com/Gimly/vscode-fortran/blob/229cddce53a2ea0b93032619efeef26376cd0d2c/src/documentSymbolProvider.ts
 import vscode = require('vscode');
+import { VbaSymbol, vbaSymbols } from './extension';
 
 export class VbaDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 
@@ -15,11 +16,14 @@ export class VbaDocumentSymbolProvider implements vscode.DocumentSymbolProvider 
         const functionRegex = /^\s*((Public|Private|Friend)\s+)?((Static)\s+)?(Function|Sub)\s+(\w+)/i;
 
         const result: vscode.SymbolInformation[] = [];
+        vbaSymbols.splice(0);
 
         for (let line = 0; line < document.lineCount; line++) {
             var text = document.lineAt(line);
             let matches = text.text.match(functionRegex);
             if (matches) {
+                const symbol = new VbaSymbol(matches[6], document.uri, text.range);
+                vbaSymbols.push(symbol);
                 result.push(
                     new vscode.SymbolInformation(
                         matches[6],
