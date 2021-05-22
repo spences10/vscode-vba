8b9c053db56ae5e642bba997e77c4cb211bf6df9
Sancarn <sancarn@hotmail.co.uk>
2021-05-22 03:36:18 +0100

    Lots of updates
    Added support for the following:
    
    `Declare`, `Lib`, `PtrSafe`, `Attribute`, `Property`, `RaiseEvent`, `Static`, `Event`
    `TypeOf`, `DoEvents`
    `MSForms`, `stdole`, `Excel`, `Office`, `VBA`, `Err`
    `WithEvents`, `ParamArray`
    `ByRef`, `ByVal`
    `VBA7`, `Win64`, `Me`
    `vbLongLong`
    `&H\d+`
    `#if`, `#elseif`, `#else`, `#end if` and `#const`
    `VERSION 1.0 CLASS`, `BEGIN ... END`
    `VB_Name`, `VB_GlobalNameSpace`, `VB_Creatable`, `VB_PredeclaredId`, `VB_Exposed`, `VB_UserMemId`, `VB_Description`
    
    Removed VB.NET only keywords `ObjectContext`, `Request`, `Response`, `Server`, `Session`
    
    Fixed a few bugs:
    * Sections of code highlighting when they weren't supposed to be
    * `as` in `Dim op As Operation` wasn't highlighted unless it was in parameters

---
+++ src/extension.js
@@ -0,0 +1,17 @@
+import * as vscode from 'vscode';
+/**
+ * 
+ * @param {vscode.ExtensionContext} context 
+ */
+export function activate(context) {
+    //Auto-completion
+    //https://github.com/microsoft/vscode-extension-samples/blob/main/completions-sample/src/extension.ts
+
+    //Auto format
+    //https://github.com/jrieken/vscode-formatter-sample
+
+    //Custom data for vb constants e.g. vbString, vbInteger etc.
+    //https://github.com/microsoft/vscode-extension-samples/tree/main/custom-data-sample
+
+
+};
